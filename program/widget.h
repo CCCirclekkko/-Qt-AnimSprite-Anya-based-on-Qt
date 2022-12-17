9cd59482b1b3bc7f668b3b667138cc4b91a96447
CCCirclekkko <93822468+CCCirclekkko@users.noreply.github.com>
2022-12-17 19:01:45 +0800

    Add files via upload

--- program/widget.h
+++ program/widget.h
@@ -4,10 +4,13 @@
 #include <QPaintEvent>
 #include <QWidget>
 #include <QRect>
+#include <QMouseEvent>
+#include <QMovie>
 
-const QSize picSize(449,386);
+const QSize startPicSize(449,386);
 const float picScale = 0.25;    //图片缩放比例
-const QRect rectWin(1345,887,picSize.width()*picScale,picSize.height()*picScale);
+//const QRect rectWin(1345,887,startPicSize.width()*picScale,startPicSize.height()*picScale);
+const QPoint startP(1345,887);
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Widget; }
@@ -22,7 +25,15 @@ public:
     //Widget(QWidget *parent = 0, Qt::WFlags flags =Qt::FramelessWindowHint|Qt::WindowStaysOnTopHint|Qt::Tool);//构造函数
     ~Widget();
 
+    QImage pic,bottom;
+    QMovie *startUp, *drag;
+
+    bool isDragging=false, dragGifShown=false;
+
     void paintEvent(QPaintEvent * event)    Q_DECL_OVERRIDE;
+    void mousePressEvent(QMouseEvent * event)    Q_DECL_OVERRIDE;
+    void mouseMoveEvent(QMouseEvent * event)    Q_DECL_OVERRIDE;
+    void mouseReleaseEvent(QMouseEvent * event)    Q_DECL_OVERRIDE;
 
 private:
     Ui::Widget *ui;
